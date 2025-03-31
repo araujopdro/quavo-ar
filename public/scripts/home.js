341136bab50768fe96006ffe5b609e77e5843235
Pedro Araujo <araujo.pdro@gmail.com>
2025-03-31 10:52:04 -0300

    paginas empreendimentos

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -5,9 +5,11 @@ console.log(model_viewer_guara)
 
 model_viewer_guara.setAttribute("src", "/assets/models/guarapuavaexteriorweb.glb")
 model_viewer_guara.setAttribute("ar", "")
+model_viewer_guara.setAttribute("disable-zoom", "")
+model_viewer_guara.setAttribute("disable-pan", "")
+model_viewer_guara.setAttribute("auto-rotate", "")
+model_viewer_guara.setAttribute("touch-action", "none")
 model_viewer_guara.setAttribute("ar-modes", "webxr scene-viewer quick-look")
-model_viewer_guara.setAttribute("camera-controls", "")
-model_viewer_guara.setAttribute("touch-action", "pan-y")
 model_viewer_guara.setAttribute("shadow-intensity", "1")
 model_viewer_guara.setAttribute("environment-image", "legacy")
 model_viewer_guara.setAttribute("camera-orbit", "45deg 65deg 2.5m")
@@ -17,9 +19,20 @@ console.log(model_viewer_moises_marx)
 
 model_viewer_moises_marx.setAttribute("src", "/assets/models/mm_exteriorv2.glb")
 model_viewer_moises_marx.setAttribute("ar", "")
+model_viewer_moises_marx.setAttribute("disable-zoom", "")
+model_viewer_moises_marx.setAttribute("disable-pan", "")
+model_viewer_moises_marx.setAttribute("auto-rotate", "")
+model_viewer_moises_marx.setAttribute("touch-action", "none")
 model_viewer_moises_marx.setAttribute("ar-modes", "webxr scene-viewer quick-look")
-model_viewer_moises_marx.setAttribute("camera-controls", "")
-model_viewer_moises_marx.setAttribute("touch-action", "pan-y")
 model_viewer_moises_marx.setAttribute("shadow-intensity", "1")
 model_viewer_moises_marx.setAttribute("environment-image", "legacy")
-model_viewer_moises_marx.setAttribute("camera-orbit", "45deg 65deg 2.5m")
+model_viewer_moises_marx.setAttribute("camera-orbit", "45deg 65deg 350m") 
+
+// ar="" ar-modes="webxr scene-viewer quick-look" 
+// camera-controls=""
+// touch-action="pan-y"
+// shadow-intensity="1"
+// environment-image="legacy" 
+// camera-orbit="45deg 65deg 45m" 
+// class="w-100 h-100 position-absolute top-0 left-0" 
+// ar-status="not-presenting"
